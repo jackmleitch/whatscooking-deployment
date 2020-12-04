704cbe82ff86dbaaf983e65a9aea923b4e2e4685
Jack <jackmleitch@gmail.com>
2020-12-04 12:07:05 -0500

    heroku

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-RECIPES_PATH = "./df_recipes.csv"
-PARSED_PATH = "./df_parsed.csv"
+RECIPES_PATH = "./input/df_recipes.csv"
+PARSED_PATH = "./input/df_parsed.csv"
 
 TFIDF_ENCODING_PATH = "./tfidf_encodings.pkl"
 TFIDF_MODEL_PATH = "./tfidf.pkl"
