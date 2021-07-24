1b2613750e77520975b525687be45ea5ee3a6ed4
jackmleitch <jackmleitch@gmail.com>
2021-07-24 22:27:34 +0100

    init

--- streamlit.py
+++ streamlit.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import SessionState
+import os
+from PIL import Image
 
 import config, rec_sys
 from ingredient_parser import ingredient_parser
@@ -22,21 +24,29 @@ def make_clickable(name, link):
 
 
 def main():
+    image = Image.open("input/wordcloud.png").resize((680, 150))
+    st.image(image)
     st.markdown("# *What's Cooking? :cooking:*")
 
     st.markdown(
         "An ML powered app by Jack Leitch <a href='https://github.com/jackmleitch/Whatscooking-' > <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/600px-Octicons-mark-github.svg.png' width='20' height='20' > </a> ",
         unsafe_allow_html=True,
     )
-    st.header("Given a list of ingredients, what different recipes can I can make?")
     st.markdown(
-        "For example, what recipes can I make with the food I have in my apartment? Find out for yourself below!"
+        "## Given a list of ingredients, what different recipes can I can make? :tomato: "
+    )
+    st.markdown(
+        "For example, what recipes can you make with the food in your apartment? :house: My ML based model will look through over 4500 recipes to find matches for you... :mag: Try it out for yourself below! :arrow_down:"
     )
 
     st.text("")
 
     session_state = SessionState.get(
-        recipe_df="", recipes="", model_computed=False, execute_recsys=False
+        recipe_df="",
+        recipes="",
+        model_computed=False,
+        execute_recsys=False,
+        recipe_df_clean="",
     )
 
     ingredients = st.text_input("Enter ingredients you would like to cook with")
@@ -49,7 +59,7 @@ def main():
             gif_runner = st.image("input/cooking_gif.gif")
         recipe = rec_sys.RecSys(ingredients)
         gif_runner.empty()
-
+        session_state.recipe_df_clean = recipe.copy()
         # link is the column with hyperlinks
         recipe["url"] = recipe.apply(
             lambda row: make_clickable(row["recipe"], row["url"]), axis=1
@@ -64,16 +74,25 @@ def main():
         # st.write("Either pick a particular recipe or see the top 5 recommendations.")
         recipe_all_box = st.selectbox(
             "Either see the top 5 recommendations or pick a particular recipe ya fancy",
-            ["Show me all!", "Just show me one please"],
+            ["Show me them all!", "Select a single recipe"],
         )
-        if recipe_all_box == "Show me all!":
+        if recipe_all_box == "Show me them all!":
             st.write(session_state.recipe_display, unsafe_allow_html=True)
         else:
-            st.selectbox("Select a delicious recipe", options=session_state.recipes)
+            selection = st.selectbox(
+                "Select a delicious recipe", options=session_state.recipes
+            )
+            selection_details = session_state.recipe_df_clean.loc[
+                session_state.recipe_df_clean.recipe == selection
+            ]
+            st.write(f"Recipe: {selection_details.recipe.values[0]}")
+            st.write(f"Ingredients: {selection_details.ingredients.values[0]}")
+            st.write(f"URL: {selection_details.url.values[0]}")
+            st.write(f"Score: {selection_details.score.values[0]}")
 
     # sidebar stuff
     with st.sidebar.beta_expander("How it works?", expanded=True):
-        st.subheader("How it works?")
+        st.markdown("## How it works? :thought_balloon:")
         st.write(
             "For an in depth overview of the ML methods used and how I created this app, two blog posts are below."
         )
