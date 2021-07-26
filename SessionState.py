dd9518d78bd39864e496d4b3a3cdeead702869cf
Jack <jackmleitch@gmail.com>
2021-07-26 16:25:25 +0100

    init

--- SessionState.py
+++ SessionState.py
@@ -24,7 +24,7 @@ try:
     from streamlit.server.Server import Server
 except Exception:
     # Streamlit >= 0.65.0
-    from streamlit.report_thread import get_report_ctx as ReportThread
+    import streamlit.report_thread as ReportThread
     from streamlit.server.server import Server
 
 
